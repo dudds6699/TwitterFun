520c0fdc97bb5cab951b53e0d1cea1c37ed45d09
dudds6699 <dudds6699@hotmail.com>
2014-11-26 01:40:42 +0000

    well here it is how i got tweets in a database for references for postgis people

--- index.js
+++ index.js
@@ -12,14 +12,11 @@ var T = new Twit({
 });
 
 
-//myConnection
+//myConnection  postgres://username:password@0.0.0.0:5432/database
 var conString = "";
 
 //my stream
 var stream = T.stream('statuses/sample');
-
-
-
 
 
 stream.on('tweet', function(tweet) {
@@ -56,27 +53,16 @@ function connectToDb(sql){
 
 
 function makeSQL(tweet){
-    //just some simple parsing for the special geometry functions i use to store the point
-    var startQuery = "st_setsrid(st_makepoint(";
     
+    //just some simple parsing for the special geometry functions i use to store the point
     //some times 3395 or 4326
-    var endQuery = "),3395)";
-    var coordinates = tweet.geo.coordinates;
     //converts the thing to sql nice stuff
-/*
-    return sql.insert()
-            .into("tweets")
-            .set("username", tweet.user.screen_name)
-            .set("Tweet", tweet.text)
-            .set("geo", startQuery + coordinates[0] + "," + coordinates[1] + endQuery,{
-                dontQuote:true
-            }).toParam();
-  */          
-            
+    var srid = 4326;  
+    var coordinates = tweet.geo.coordinates;
     var query =
         {
-				text : "INSERT INTO tweets (username, tweet, geo) Values ($1,$2,st_setsrid(st_makepoint($3,$4),3395))",
-				values:[tweet.user.screen_name, tweet.text, coordinates[0] , coordinates[1]  ]
+				text : "INSERT INTO tweets (username, tweet, geo) Values ($1,$2,st_setsrid(st_makepoint($3,$4),"+srid+"))",
+				values:[tweet.user.screen_name, tweet.text, coordinates[1] , coordinates[0]  ]
         };
         
     return query;
